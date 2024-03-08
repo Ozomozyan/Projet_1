2278bdb0abb089e67002a85fc3009aa4b751763d
Ozomozyan <bozuesa@gmail.com>
2024-03-08 17:49:58 +0100

    Your commit message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Bonjour, le monde!'
+    return 'Im testing my regular tasks'
 
 if __name__ == '__main__':
     app.run(debug=True)
