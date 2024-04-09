701b46c0215e41ac347ebd331631619e5043b7f8
Ozomozyan <bozuesa@gmail.com>
2024-04-09 13:33:20 +0200

    Admin update feautures

--- app.py
+++ app.py
@@ -128,18 +128,15 @@ def admin_update_user():
         login = request.form['login']
         nom = request.form['nom']
         prenom = request.form['prenom']
-        password = request.form['password'] if request.form['password'] else None
+        password = request.form['password'] if 'password' in request.form and request.form['password'] else None
         role = request.form['role']
-
-        success = user_management.admin_update_user_info(login, nom, prenom, password, role)
-        if success:
-            # If using AJAX, return JSON response instead of redirecting
-            return jsonify({"success": "User information updated successfully.", "login": login}), 200
+        if user_management.admin_update_user_info(login, nom, prenom, password, role):
+            flash("User information updated successfully.", "success")
         else:
-            return jsonify({"error": "Failed to update user information."}), 500
+            flash("Failed to update user information.", "error")
     else:
-        return jsonify({"error": "Unauthorized action."}), 403
-
+        flash("Unauthorized action.", "error")
+    return redirect(url_for('admin_dashboard'))
 
 
 @app.route('/admin/update_user_info', methods=['POST'])
